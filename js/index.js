eff2e5efe68998568bd8031ddcf86ac56e902ec1
Kristin Tobin <ktobin1@bu.edu>
2021-12-14 19:33:20 -0500

    commit

--- js/index.js
+++ js/index.js
@@ -81,3 +81,27 @@ function getData() {
          document.body.appendChild(table);
        });
  };
+//Photo Gallery 
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs img');
+const opacity = 0.4;
+
+imgs [0].style.opactiy = opacity;
+
+imgs.forEach(img => img.addEventListener('click', imgClick)
+  );
+
+  function imgClick(e) {
+    //Reset opacity
+    imgs.forEach(img => (img.style.opacity = 1));
+    //Change current img to source of clicked
+    current.src = e.target.src;
+    // Add fade in 
+    current.classList.add('fade-in');
+    //remove fade-in class
+    setTimeout(() => current.classList.remove('fade-in'), 500);
+    //Change opacity
+    e.target.style.opacity = opacity;
+
+  }
+  
